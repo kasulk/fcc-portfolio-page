8a02deb3f2d0a9918b840b4f4637da7fadd5ad37
kasi <danielkaser@mailbox.org>
2023-08-22 14:55:21 +0200

    add timeout to fetched data to give fade time to do its magic

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// function begruesse() {
-//   alert("Hallo von meiner externen JavaScript-Datei!");
-// }
-
 // const demo = {
 //   categories: [],
 //   created_at: "2020-01-05 13:42:24.142371",
@@ -13,27 +9,36 @@
 //     "Chuck Norris can climb Mount Everest with his eyes closed and his arms tied behind his back. He does that every morning to worm-up.",
 // };
 
-// const url = "https://api.chucknorris.io/jokes/random";
+const url = "https://api.chucknorris.io/jokes/random";
 const quoteElement = document.querySelector("#quote q");
 let savedScrollPosition = 0;
 
 function fetchAndRender() {
-  //   const savedScrollPosition = quoteElement.scrollTop;
   savedScrollPosition = window.scrollY;
 
-  fetch("https://api.chucknorris.io/jokes/random")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.value);
-      quoteElement.innerHTML = `${data.value[0].toUpperCase()}${data.value.substring(
-        1
-      )}`;
-      window.scrollTo(0, savedScrollPosition);
-      //   quoteElement.scrollTop = savedScrollPosition;
-    })
-    .catch((error) => {
-      console.error("Error fetching the data:", error);
-    });
+  quoteElement.classList.add("fade");
+
+  // give the fade a second (and a half) to do its magic
+  setTimeout(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        quoteElement.innerHTML = `${
+          //   processFetchedData(data)
+          data.value[0].toUpperCase() + data.value.substring(1)
+        }`;
+        // go back to previously saved scroll position
+        window.scrollTo(0, savedScrollPosition);
+        quoteElement.classList.remove("fade");
+      })
+      .catch((error) => {
+        console.error("Error fetching the data:", error);
+      });
+  }, 1500);
 }
 
-setInterval(fetchAndRender, 5000);
+function processFetchedData(data) {
+  data.value[0].toUpperCase() + data.value.substring(1);
+}
+
+setInterval(fetchAndRender, 1000 * 10);
