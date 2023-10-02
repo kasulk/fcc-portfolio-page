09d8ca401c07073f79080dd29aa8ae407e9c8ef4
kasi <danielkaser@mailbox.org>
2023-10-02 15:28:54 +0200

    add possibiliy to close modal window on click outside of it

--- modals.js
+++ modals.js
@@ -1,11 +1,23 @@
+const body = document.body;
+
 const imprintButton = document.querySelector("#legal button:first-child");
-const imprintModal = document.getElementById("modal-imprint");
-const imprintClose = document.getElementById("close");
+const imprintModal = document.querySelector("#modal-imprint");
+const imprintClose = document.querySelector("#close");
 
 imprintButton.addEventListener("click", () => {
   imprintModal.style.display = "block";
+  body.style.overflow = "hidden";
 });
 
 imprintClose.addEventListener("click", () => {
   imprintModal.style.display = "none";
+  body.style.overflow = "auto";
+});
+
+// close modal window on click outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === imprintModal) {
+    imprintModal.style.display = "none";
+    body.style.overflow = "auto";
+  }
 });
