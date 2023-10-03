3cedd25e4908cfc8e2f627d54b6e5e4c28756bcc
kasi <danielkaser@mailbox.org>
2023-10-03 17:12:56 +0200

    refactor modal logic; create dedicated func to toggle modal display and body freeze

--- js/modals.js
+++ js/modals.js
@@ -1,85 +1,31 @@
 const body = document.body;
-
-// // imprint modal
-// const imprintLink = document.querySelector("#legal a:first-child");
-// const imprintModal = document.querySelector("#modal-imprint");
-// const imprintClose = document.querySelector("#close-imprint");
-
-// imprintLink.addEventListener("click", () => {
-//   imprintModal.style.display = "block";
-//   body.style.overflow = "hidden";
-// });
-
-// imprintClose.addEventListener("click", () => {
-//   imprintModal.style.display = "none";
-//   body.style.overflow = "auto";
-// });
-
-// // close modal window on click outside of it
-// // window.addEventListener("click", (event) => {
-// //   if (event.target === imprintModal) {
-// //     imprintModal.style.display = "none";
-// //     body.style.overflow = "auto";
-// //   }
-// // });
-
-// // privacy modal
-// const privacyLink = document.querySelector("#legal a:last-child");
-// const privacyModal = document.querySelector("#modal-privacy");
-// const privacyClose = document.querySelector("#close-privacy");
-
-// privacyLink.addEventListener("click", () => {
-//   privacyModal.style.display = "block";
-//   body.style.overflow = "hidden";
-// });
-
-// privacyClose.addEventListener("click", () => {
-//   privacyModal.style.display = "none";
-//   body.style.overflow = "auto";
-// });
-
-// // close modal window on click outside of it
-// window.addEventListener("click", (event) => {
-//   if (event.target === imprintModal) {
-//     imprintModal.style.display = "none";
-//     body.style.overflow = "auto";
-//   }
-
-//   if (event.target === privacyModal) {
-//     privacyModal.style.display = "none";
-//     body.style.overflow = "auto";
-//   }
-// });
-
-//! hot stuff
 const legalLinks = document.querySelectorAll("#legal a");
-// console.log(legalLinks);
 const modals = document.querySelectorAll(".modal");
-// console.log(modals);
 const modalCloseButtons = document.querySelectorAll(".close");
-// console.log(modalCloseButtons);
 
+//
 legalLinks.forEach((legalLink, i) => {
+  // show modal on link click and freeze body in bg
   legalLink.addEventListener("click", () => {
-    console.log("yes!");
-    modals[i].style.display = "block";
-    body.style.overflow = "hidden";
+    setModalDisplayAndBodyFreeze(modals[i], "block", "hidden");
   });
+  // close modal on click on close button and defreeze body
   modalCloseButtons[i].addEventListener("click", () => {
-    console.log("close button geklcikt!");
-    modals[i].style.display = "none";
-    body.style.overflow = "auto";
+    setModalDisplayAndBodyFreeze(modals[i], "none", "auto");
   });
 });
 
-// window.addEventListener("click", (event) => {
-//   if (event.target === imprintModal) {
-//     imprintModal.style.display = "none";
-//     body.style.overflow = "auto";
-//   }
+// close modal on click outside of modal-content
+window.addEventListener("click", (event) => {
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      setModalDisplayAndBodyFreeze(modal, "none", "auto");
+    }
+  });
+});
 
-//   if (event.target === privacyModal) {
-//     privacyModal.style.display = "none";
-//     body.style.overflow = "auto";
-//   }
-// });
+//
+function setModalDisplayAndBodyFreeze(modal, modalDisplayStyle, bodyOverflow) {
+  modal.style.display = modalDisplayStyle;
+  body.style.overflow = bodyOverflow;
+}
