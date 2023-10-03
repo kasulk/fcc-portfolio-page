dc41cbb88c3620fe5eef334fa69365ec887a05b9
kasi <danielkaser@mailbox.org>
2023-10-03 16:46:51 +0200

    refactor modal logic; create array and loop

--- js/modals.js
+++ js/modals.js
@@ -1,47 +1,85 @@
 const body = document.body;
 
-// imprint modal
-const imprintButton = document.querySelector("#legal a:first-child");
-const imprintModal = document.querySelector("#modal-imprint");
-const imprintClose = document.querySelector("#close-imprint");
+// // imprint modal
+// const imprintLink = document.querySelector("#legal a:first-child");
+// const imprintModal = document.querySelector("#modal-imprint");
+// const imprintClose = document.querySelector("#close-imprint");
 
-imprintButton.addEventListener("click", () => {
-  imprintModal.style.display = "block";
-  body.style.overflow = "hidden";
-});
+// imprintLink.addEventListener("click", () => {
+//   imprintModal.style.display = "block";
+//   body.style.overflow = "hidden";
+// });
 
-imprintClose.addEventListener("click", () => {
-  imprintModal.style.display = "none";
-  body.style.overflow = "auto";
-});
+// imprintClose.addEventListener("click", () => {
+//   imprintModal.style.display = "none";
+//   body.style.overflow = "auto";
+// });
 
-// close modal window on click outside of it
-window.addEventListener("click", (event) => {
-  if (event.target === imprintModal) {
-    imprintModal.style.display = "none";
+// // close modal window on click outside of it
+// // window.addEventListener("click", (event) => {
+// //   if (event.target === imprintModal) {
+// //     imprintModal.style.display = "none";
+// //     body.style.overflow = "auto";
+// //   }
+// // });
+
+// // privacy modal
+// const privacyLink = document.querySelector("#legal a:last-child");
+// const privacyModal = document.querySelector("#modal-privacy");
+// const privacyClose = document.querySelector("#close-privacy");
+
+// privacyLink.addEventListener("click", () => {
+//   privacyModal.style.display = "block";
+//   body.style.overflow = "hidden";
+// });
+
+// privacyClose.addEventListener("click", () => {
+//   privacyModal.style.display = "none";
+//   body.style.overflow = "auto";
+// });
+
+// // close modal window on click outside of it
+// window.addEventListener("click", (event) => {
+//   if (event.target === imprintModal) {
+//     imprintModal.style.display = "none";
+//     body.style.overflow = "auto";
+//   }
+
+//   if (event.target === privacyModal) {
+//     privacyModal.style.display = "none";
+//     body.style.overflow = "auto";
+//   }
+// });
+
+//! hot stuff
+const legalLinks = document.querySelectorAll("#legal a");
+// console.log(legalLinks);
+const modals = document.querySelectorAll(".modal");
+// console.log(modals);
+const modalCloseButtons = document.querySelectorAll(".close");
+// console.log(modalCloseButtons);
+
+legalLinks.forEach((legalLink, i) => {
+  legalLink.addEventListener("click", () => {
+    console.log("yes!");
+    modals[i].style.display = "block";
+    body.style.overflow = "hidden";
+  });
+  modalCloseButtons[i].addEventListener("click", () => {
+    console.log("close button geklcikt!");
+    modals[i].style.display = "none";
     body.style.overflow = "auto";
-  }
+  });
 });
 
-// privacy modal
-const privacyButton = document.querySelector("#legal a:last-child");
-const privacyModal = document.querySelector("#modal-privacy");
-const privacyClose = document.querySelector("#close-privacy");
+// window.addEventListener("click", (event) => {
+//   if (event.target === imprintModal) {
+//     imprintModal.style.display = "none";
+//     body.style.overflow = "auto";
+//   }
 
-privacyButton.addEventListener("click", () => {
-  privacyModal.style.display = "block";
-  body.style.overflow = "hidden";
-});
-
-privacyClose.addEventListener("click", () => {
-  privacyModal.style.display = "none";
-  body.style.overflow = "auto";
-});
-
-// close modal window on click outside of it
-window.addEventListener("click", (event) => {
-  if (event.target === privacyModal) {
-    privacyModal.style.display = "none";
-    body.style.overflow = "auto";
-  }
-});
+//   if (event.target === privacyModal) {
+//     privacyModal.style.display = "none";
+//     body.style.overflow = "auto";
+//   }
+// });
